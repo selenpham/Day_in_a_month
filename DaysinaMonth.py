30748d9f40e96695864f94d54598fd34e5b33549
Hai Thanh Pham <59055154+selenpham@users.noreply.github.com>
2023-04-14 08:00:55 +0700

    Update DaysinaMonth.py

--- DaysinaMonth.py
+++ DaysinaMonth.py
@@ -1,11 +1,3 @@
-# Days in a Month
-# Write a function that determines how many days there are in a particular month.
-# Your function will take two parameters: The month as an integer between 1 and 12, 
-# and the year as a four digit integer. Ensure that your function reports the correct 
-# number of days in February for leap years. Include a main program that reads a month 
-# and year from the user and displays the number of days in that month. You may find 
-# your solution to Exercise 57 helpful when solving this problem. 
-
 # kiểm tra dk các tháng, tháng 2 có đk đặc biệt lq đến năm nhuận, các tháng có 30 ngày, các tháng có 31 ngày
 def days_in_month(month, year):
     if month == 2:
